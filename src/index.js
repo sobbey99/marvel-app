397a7caed2eca0c4c5386b07da6860a03b8f24db
sobbey99 <sobbey99@gmail.com>
2023-01-21 14:45:05 +0200

    GET request to Marvel API Component Done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
+import MarvelSerivce from './services/MarvelService';
 import './style/style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const marvelService = new MarvelSerivce();
 
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => {
+  console.log(item.name)
+}));
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ src/services/MarvelService.js
@@ -0,0 +1,27 @@
+
+
+class MarvelSerivce {
+    _apiBase ='https://gateway.marvel.com/v1/public/';
+    _apiKey = 'apikey=f0359cc145966ec5264a721a0d79a587';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
+    }
+
+    
+}
+
+export default MarvelSerivce;
