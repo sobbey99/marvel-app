9c3afc9af0c199390ec71ef708b6818b7c6c6cb2
sobbey99 <sobbey99@gmail.com>
2023-01-21 20:23:35 +0200

    RandomChar component done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
-import MarvelSerivce from './services/MarvelService';
 import './style/style.scss';
-
-const marvelService = new MarvelSerivce();
-
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => {
-  console.log(item.name)
-}));
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
