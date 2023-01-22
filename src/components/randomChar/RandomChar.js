40f98d5a70732221132c739ce5f8ae730df83265
sobbey99 <sobbey99@gmail.com>
2023-01-22 10:56:48 +0200

    homework lesson 152

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -26,6 +26,12 @@ class RandomChar extends Component{
         })
     }
     
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
     onError = () => {
         this.setState({
             loading: false,
@@ -35,6 +41,7 @@ class RandomChar extends Component{
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 -1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
