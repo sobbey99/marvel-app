90bb844f8e312026a1486953cb11c00d4f91da91
sobbey99 <sobbey99@gmail.com>
2023-01-22 11:45:51 +0200

    ErrorBoundary Component

---
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import ErrorMessage from "../errorMessage/errorMessage";
+
+class ErrorBoundary extends Component {
+    state = {
+        error: false
+    }
+
+    
+
+    componentDidCatch(err, errorInfo) {
+        console.log(err, errorInfo);
+
+        this.setState({
+            error: true
+        })
+    }
+
+    render() {
+        if (this.state.error) {
+            return <ErrorMessage/>
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
